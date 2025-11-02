e82abf4252af34eeb3b0ca98177ab60d4b859495
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-11-02 17:41:32 +0530

    Update 02. Multidimensional Array.py

--- 02. Multidimensional Array.py
+++ 02. Multidimensional Array.py
@@ -5,3 +5,15 @@ print(np.ndim(array))
 print("1 Dimensional Array")
 array = np.array(['A','B','C','D','E'])
 print(np.ndim(array))
+
+print("2 Dimensional Array")
+array = np.array([['A','B','C','D','E'],['F','G','H','I','J']])
+print(np.ndim(array))
+print(np.shape(array))
+print("\n")
+
+print("Accessing element with index")
+print(array[0][3]) # Different ways to access element
+print(array[1,3])
+
+print(array[0,0]+array[1,1])
