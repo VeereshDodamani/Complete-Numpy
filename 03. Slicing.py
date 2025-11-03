baf64d02f6271b5a598eb8db52e6568c834b9bc0
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-11-03 10:55:40 +0530

    Update 03. Slicing.py

--- 03. Slicing.py
+++ 03. Slicing.py
@@ -25,4 +25,28 @@ print("\n")
 
 print("Using start:end:step command")
 # Step means skip at rotation
+print("Accessing 0:4:2")
 print(array[0:4:2]) # 2 means skip the 2nd row which comes
+print("Accessing 0:4:3")
+print(array[0:4:3]) # 3 means skip the 3rd row which comes
+
+print("\n")
+print("Using ::step command")
+print("Reversing the array with ::-1")
+print(array[::-1])
+print("Reversing the array with ::-2")
+print(array[::-2])
+print("\n")
+
+print("Accessing all row, 0th column")
+print(array[:,0])
+print("Accessing all row, 1st column")
+print(array[:,1])
+print("\n")
+
+print("Accessing all row, 0th-2nd column")
+print(array[:,0:3])
+print("Accessing 1st-2nd row, 1st-2nd column")
+print(array[0:2,0:2])
+print("Accessing 3rd-4th row, 3rd-4th column")
+print(array[2:,2:])
