9126c6cf27b101d6459c3f347217f1fa88a36443
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-11-04 12:17:31 +0530

    Update 04. Arithmetic.py

--- 04. Arithmetic.py
+++ 04. Arithmetic.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# Scalar arithmetics
 print("Scalar arithmetics")
 array = np.array([1,2,3])
 print("Actual Array")
@@ -12,3 +13,18 @@ print("Multiply by 2")
 print(array * 2)
 print("Divide by 4")
 print(array / 4)
+
+
+# Vectorised Math Function
+print("Vectorised Math Function")
+array = np.array([1.01,2.52,3.34])
+print("Actual Array")
+print(array)
+print("Sqrt of the array")
+print(np.sqrt(array))
+print("Round off array")
+print(np.round(array))
+print("Round off array to floor")
+print(np.floor(array))
+print("Round off array to ceil")
+print(np.ceil(array))
