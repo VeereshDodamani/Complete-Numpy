288d79005662d08ea0a180fe518f000aadbaa37b
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-11-04 12:06:31 +0530

    Create 04. Arithmetic.py

---
+++ 04. Arithmetic.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+print("Scalar arithmetics")
+array = np.array([1,2,3])
+print("Actual Array")
+print(array)
+print("Adding 1")
+print(array + 1)
+print("Substracting 1")
+print(array - 1)
+print("Multiply by 2")
+print(array * 2)
+print("Divide by 4")
+print(array / 4)
