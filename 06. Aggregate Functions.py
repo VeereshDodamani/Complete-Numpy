93fa662eb92dcd0ecc5c32316667ea9f21d10738
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-11-06 22:12:29 +0530

    Update 06. Aggregate Functions.py

--- 06. Aggregate Functions.py
+++ 06. Aggregate Functions.py
@@ -1,7 +1,8 @@
 import numpy as np
 # Aggregate function = summarise data and typically return a single value
 
-array = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+array = np.array([[1,2,3,4,5],
+                  [6,7,8,9,10]])
 print("Actual Array")
 print(array)
 print("Sum of the array: ",np.sum(array))
@@ -10,3 +11,9 @@ print("Standard deviation of the array: ",np.std(array))
 print("Variance of the array: ",np.var(array))
 print("Minimum element of the array: ",np.min(array))
 print("Maximum element of the array: ",np.max(array))
+print("Smallest element index in the array: ",np.argmin(array))
+print("Largest element index in the array: ",np.argmax(array))
+print("\n")
+
+print("Sum of the array column wise: ",np.sum(array, axis=0))
+print("Sum of the array row wise: ",np.sum(array, axis=1))
