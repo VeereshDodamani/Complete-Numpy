d2c66fbe13f0630bc9b1a3ac53b448e4bd7182e0
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-11-09 11:03:03 +0530

    Update 08. Random Numbers.py

--- 08. Random Numbers.py
+++ 08. Random Numbers.py
@@ -7,3 +7,10 @@ print("\n")
 
 print("Generate random number between 0-1:",np.random.uniform())
 print("Generate random number between 0-1:\n",np.random.uniform(low=1, high=10, size=(2,2)))
+print("\n")
+
+rng = np.random.default_rng()
+array = np.array([1,2,3,4,5])
+print("Actual array: ",array)
+rng.shuffle(array)
+print(array)
